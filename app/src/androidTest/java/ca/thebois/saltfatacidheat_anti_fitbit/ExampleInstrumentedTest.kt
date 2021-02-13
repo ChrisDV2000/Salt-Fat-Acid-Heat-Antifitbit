d3514907ad382150260e5b572c47cabd5b11607c
Bryan Niwa <bryan_niwa@sfu.ca>
2021-02-13 12:43:43 -0800

    Initial commit

---
+++ app/src/androidTest/java/ca/thebois/saltfatacidheat_anti_fitbit/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package ca.thebois.saltfatacidheat_anti_fitbit
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("ca.thebois.saltfatacidheat_anti_fitbit", appContext.packageName)
+    }
+}
